ba9a1d34538dfac8eb39a5202d005c67e251e1cb
Ruchirakavinda <ruchirakvnd@gmail.com>
2021-11-12 22:53:26 +0530

    01

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# Note-Keep---MERN-App-Front-End
+Application for store notes 
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     Consequat labore deserunt excepteur irure ex duis Lorem eu duis est.
+    </div>
+  );
+}
+
+export default App;
